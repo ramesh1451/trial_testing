092e273ef16a1552dddd419f7a1fc64ec51c588c
Ramesh Choyal <krsna@Rameshs-MacBook-Air.local>
2022-12-15 10:19:51 +0530

    updated the file

--- firstcode.java
+++ firstcode.java
@@ -5,6 +5,7 @@ class secondcode
     public static void main(String a[])
     {
         System.out.print("Hello World");
+        System.out.print("This is updatd and going to push to github");
     }
     
 
